8c00f792371997293739dd7c94ca7cfcd4fa42e2
Cairo Araujo <cairobezerra@hotmail.com>
2024-12-05 21:00:23 -0300

    created users service

---
+++ game-shelf/src/services/users/usersService.tsx
@@ -0,0 +1,26 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+  }
+  
+  export async function fetchUsers(): Promise<User[]> {
+    try {
+      const response = await fetch("https://games-shelf-api.fly.dev/users", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error fetching users: ${response.statusText}`);
+      }
+  
+      const data: User[] = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+      return [];
+    }
+  }
